70ad9214cd7567b795eef84c97da84f8980d37d9
Rikito Sakurada <rikito82k@gmail.com>
2023-10-13 22:11:20 +0900

    型指定していおないのでエラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ inputValue, setinputValue ] = useState("")
+  const [ todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
+
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with TypeScript</h2>
         <form onSubmit={() => {}}>
-          <input className='inpuText' onChange={() => {}} type="text" />
+          <input className='inpuText' onChange={(e) => handleChange(e)} type="text" />
           <input type="submit" value="作成" className='submitButtom' />
         </form>
       </div>
